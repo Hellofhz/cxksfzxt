b128bd6f3717d6c8c377e86fa1ee09d454b90e4b
Hellofhz <fhzit08@gmail.com>
2025-04-27 17:32:43 +0800

    Update main.js

--- exam/Scripts/main.js
+++ exam/Scripts/main.js
@@ -14,25 +14,4 @@ document.addEventListener("DOMContentLoaded", () => {
             errorSystem.show('全屏切换失败: ' + e.message);
         }
     });
-
-    // 页面加载完成后自动点击全屏按钮
-    fullscreenBtn.click();
 });
-
-// 页面加载完成后自动全屏
-window.onload = function() {
-    function enterFullScreen() {
-        if (document.documentElement.requestFullscreen) {
-            document.documentElement.requestFullscreen();
-        } else if (document.documentElement.mozRequestFullScreen) { // Firefox
-            document.documentElement.mozRequestFullScreen();
-        } else if (document.documentElement.webkitRequestFullscreen) { // Chrome, Safari and Opera
-            document.documentElement.webkitRequestFullscreen();
-        } else if (document.documentElement.msRequestFullscreen) { // IE/Edge
-            document.documentElement.msRequestFullscreen();
-        }
-    }
-
-    // 延迟执行以确保在 浏览器 中生效
-    setTimeout(enterFullScreen, 1000);
-};
